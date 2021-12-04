b90ba9ccd4e3595dfc1f8211418629f25c7883d2
murxy <75093384+murxy@users.noreply.github.com>
2021-12-04 12:51:15 +0300

    Update Sketch
    Make it more understandble

--- examples/Blinker/Blinker.ino
+++ examples/Blinker/Blinker.ino
@@ -3,18 +3,18 @@
 #include <SAA1064.h>
 
 
-SAA1064 Display1 = SAA1064(0x3B);
-SAA1064 Display2 = SAA1064(0x38);
+SAA1064 LeftColumn = SAA1064(0x38);
+SAA1064 RightColumn = SAA1064(0x3B);
 
 void setup() {
-Display1.begin();
-Display2.begin();
+LeftColumn.begin();
+RightColumn.begin();
 }
 
 void loop() {
 for (uint8_t i = 0; i < 101; i++){
-  Display1.value(i, 1);
-  Display2.value(100-i, 1);
+  LeftColumn.value(i, 1);
+  RightColumn.value(100-i, 1);
   delay(10);
 }
 }
